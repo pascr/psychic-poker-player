44555d14f69ef85072140842e65ecb91471f1664
pascr <pascal@sliceofcode.net>
2014-02-13 13:59:56 +0100

    adding parser test

---
+++ src/main/java/net/sliceofcode/psychic/poker/player/LineParser.java
@@ -0,0 +1,21 @@
+package net.sliceofcode.psychic.poker.player;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+/**
+ * Created by PRodriguez on 13.02.14.
+ */
+public class LineParser {
+
+    /**
+     * Parses a line, validates and extract cards
+     * @param inputLine
+     *
+     */
+    public Set<String> parseLine(String inputLine) {
+        return Collections.emptySet();
+
+    }
+}
--- src/test/java/net/sliceofcode/psychic/poker/player/PokerFileParserTest.java
+++ src/test/java/net/sliceofcode/psychic/poker/player/PokerFileParserTest.java
@@ -1,16 +1,36 @@
 package net.sliceofcode.psychic.poker.player;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.*;
+
 /**
- * Created by PRodriguez on 13.02.14.
+ * Created by pascr on 13.02.14.
  */
 public class PokerFileParserTest
-
 {
+    private static String A_VALID_INPUT_LINE= "TH JH QC DQ QS QH KH AH 2S 6S";
+    private static String AN_INVALID_INPUT_LINE= "XX XX XX XX XX XX XX XX XX XX";
+
+    private LineParser parser;
+    @Before
+    public void init()
+    {
+        LineParser parser = new LineParser();
+    }
+
     @Test
     public void should_parse_one_line_and_extract_fields()
     {
-        
+        parser.parseLine(A_VALID_INPUT_LINE);
+        assertTrue(false);
+    }
+
+    @Test (expected = InvalidLineException.class)
+    public void should_fail_if_line_is_in_wrong_format()
+    {
+        parser.parseLine(AN_INVALID_INPUT_LINE);
+        assertTrue(false);
     }
 }
