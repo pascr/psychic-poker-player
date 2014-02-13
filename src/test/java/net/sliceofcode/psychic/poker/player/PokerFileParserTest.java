e3ec8cca6df84cd7d09920ded9bc8dafe0b82802
pascr <pascal@sliceofcode.net>
2014-02-13 13:50:36 +0100

    initial commit

---
+++ src/test/java/net/sliceofcode/psychic/poker/player/PokerFileParserTest.java
@@ -0,0 +1,16 @@
+package net.sliceofcode.psychic.poker.player;
+
+import org.junit.Test;
+
+/**
+ * Created by PRodriguez on 13.02.14.
+ */
+public class PokerFileParserTest
+
+{
+    @Test
+    public void should_parse_one_line_and_extract_fields()
+    {
+        
+    }
+}
